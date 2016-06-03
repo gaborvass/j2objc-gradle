81f98eb82be0db17d907c52284840b9ef5cf657b
Gabor Vass <gabor@gramble.com>
2016-06-03 14:25:35 +0200

    change required version number change package name

--- src/main/groovy/com/github/j2objccontrib/j2objcgradle/J2objcConfig.groovy
+++ src/main/groovy/com/github/j2objccontrib/j2objcgradle/J2objcConfig.groovy
@@ -76,7 +76,7 @@ class J2objcConfig {
     /**
      * Exact required version of j2objc.
      */
-    String j2objcVersion = '0.9.8.2.1'
+    String j2objcVersion = '1.0.2'
 
     /**
      * Don't verify J2ObjC binaries.  Useful for testing and power-users
